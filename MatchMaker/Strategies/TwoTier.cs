85dce2b8af8dd24dda9adf68939e2f1236f7092c
Steve Kuo <kuoman@gmail.com>
2021-03-06 14:27:42 -0800

    Added in tiertwo strategy

---
+++ MatchMaker/Strategies/TwoTier.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using MatchMaker.Data_Bags;
+
+namespace MatchMaker.Strategies
+{
+    public class TwoTier : IStrategy
+    {
+        private readonly int _tier;
+
+        public TwoTier(int tier)
+        {
+            _tier = tier;
+        }
+
+        public IBattle CreateBattle(List<QueueItem> queueItems)
+        {
+            BattleReady battleReady = new BattleReady();
+
+            List<QueueItem> sameTierTanks = SortForTier(queueItems, _tier);
+
+            int modSameTierTanks = (sameTierTanks.Count / 2) * 2;
+            for (int i = 0; i < modSameTierTanks ; i = i + 2)
+            {
+                battleReady.AddQueueItemToTeamA(sameTierTanks[i]);
+                battleReady.AddQueueItemToTeamB(sameTierTanks[i + 1]);
+            }
+
+            if (!battleReady.IsReadyToFight())
+            {
+                List<QueueItem> fallBackTierTanks = GetFallbackTierTanks(queueItems);
+
+                int modNextLowerTierTanks = (fallBackTierTanks.Count / 2) * 2;
+                for (int i = 0; i < modNextLowerTierTanks; i = i + 2)
+                {
+                    if (battleReady.IsReadyToFight()) break;
+
+                    battleReady.AddQueueItemToTeamA(fallBackTierTanks[i]);
+                    battleReady.AddQueueItemToTeamB(fallBackTierTanks[i + 1]);
+                }
+            }
+
+            if (13 >= queueItems.Count) return new BattleNotReady();
+
+            return battleReady;
+        }
+
+        private List<QueueItem> GetFallbackTierTanks(List<QueueItem> queueItems)
+        {
+            if (_tier == 1)
+            {
+                return SortForTier(queueItems, _tier + 1);
+            }
+
+            return SortForTier(queueItems, _tier - 1); ;
+        }
+
+        private List<QueueItem> SortForTier(List<QueueItem> queueItems, int tier)
+        {
+            List<QueueItem> sameTierTanks = new List<QueueItem>();
+
+            foreach (QueueItem queueItem in queueItems)
+            {
+                if (queueItem.IsTier(tier)) sameTierTanks.Add(queueItem);
+            }
+
+            return sameTierTanks;
+        }
+    }
+}
--- MatchMakerTests/Strategies/TwoTierTests.cs
+++ MatchMakerTests/Strategies/TwoTierTests.cs
@@ -31,25 +31,97 @@ namespace MatchMakerTests.Strategies
             battleReady.Should().NotBeNull();
         }
 
+        [TestMethod]
+        public void ShouldNotBeBattleReadyIfFewerThan14Tanks()
+        {
+            // arrange
+            IStrategy strategy = new TwoTier(5);
+
+            List<QueueItem> queueItems = new List<QueueItem>();
+            for (int i = 0; i < 3; i++)
+            {
+                queueItems.Add(CreateQueueItem(5));
+                queueItems.Add(CreateQueueItem(6));
+            }
+
+            // act
+            IBattle battleReady = strategy.CreateBattle(queueItems);
+
+            // assert
+            battleReady.IsReadyToFight().Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void ShouldCreateBattleReadyReadyToFightWith14QueueItemsOfTwoTiers6()
+        {
+            // arrange
+            IStrategy strategy = new TwoTier(6);
+
+            List<QueueItem> queueItems = new List<QueueItem>();
+
+            queueItems = AddGivenNumberOfTanksOfTier(4, 1, queueItems);
+            queueItems = AddGivenNumberOfTanksOfTier(7, 6, queueItems);
+            queueItems = AddGivenNumberOfTanksOfTier(8, 5, queueItems);
+
+            // act
+            IBattle battleReady = strategy.CreateBattle(queueItems);
+
+            // assert
+            battleReady.IsReadyToFight().Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void ShouldCreateBattleReadyReadyToFightWith14QueueItemsOfTwoTiers7()
+        {
+            // arrange
+            IStrategy strategy = new TwoTier(7);
+
+            List<QueueItem> queueItems = new List<QueueItem>();
+
+            queueItems = AddGivenNumberOfTanksOfTier(4, 2, queueItems);
+            queueItems = AddGivenNumberOfTanksOfTier(7, 7, queueItems);
+            queueItems = AddGivenNumberOfTanksOfTier(8, 6, queueItems);
+
+            // act
+            IBattle battleReady = strategy.CreateBattle(queueItems);
+
+            // assert
+            battleReady.IsReadyToFight().Should().BeTrue();
+        }
+
+
+        [TestMethod]
+        public void ShouldCreateBattleReadyReadyToFightWith14QueueItemsOfTwoTiers1()
+        {
+            // arrange
+            IStrategy strategy = new TwoTier(1);
+
+            List<QueueItem> queueItems = new List<QueueItem>();
+
+            queueItems = AddGivenNumberOfTanksOfTier(4, 6, queueItems);
+            queueItems = AddGivenNumberOfTanksOfTier(7, 1, queueItems);
+            queueItems = AddGivenNumberOfTanksOfTier(8, 2, queueItems);
+
+            // act
+            IBattle battleReady = strategy.CreateBattle(queueItems);
+
+            // assert
+            battleReady.IsReadyToFight().Should().BeTrue();
+        }
 
         private static QueueItem CreateQueueItem(int tier)
         {
             return new QueueItem(new Player(1), new Tank(tier));
         }
-    }
 
-    public class TwoTier : IStrategy
-    {
-        private readonly int _tier;
-
-        public TwoTier(int tier)
+        private static List<QueueItem> AddGivenNumberOfTanksOfTier(int numberToAdd, int tier, List<QueueItem> list)
         {
-            _tier = tier;
-        }
+            for (int i = 0; i < numberToAdd; i++)
+            {
+                list.Add(CreateQueueItem(tier));
+            }
 
-        public IBattle CreateBattle(List<QueueItem> queueItems)
-        {
-            return new BattleReady();
+            return list;
         }
     }
 }
