145ca9ff555bdd2866982868b369018759642a53
Steve Kuo <kuoman@gmail.com>
2021-04-07 11:14:13 -0700

    fixed bug with removing tanks from QueueItems but not adding them to the battle

--- MatchMaker/BattleFactory.cs
+++ MatchMaker/BattleFactory.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using MatchMaker.Data_Bags;
 using MatchMaker.Strategies;
 
@@ -9,9 +8,22 @@ namespace MatchMaker
     {
         public static IBattle Create(IStrategy strategy, QueueItems queueItems)
         {
+            return PopulateBattleByMatches(strategy, queueItems, new BattleReady(), 7);
+        }
+
+        private static readonly List<string> TankClasses = new List<string> { "Heavy", "Medium", "TankDestroyer", "Light" };
+        private static readonly int MaxTanksOfSameType = 2;
+        public static IBattle CreateFillBattle(IStrategy strategy, IRandom random, QueueItems queueItems)
+        {
+            List<string> shuffledTankClasses = random.Shuffle(TankClasses);
+
             IBattle battleReady = new BattleReady();
 
-            battleReady = PopulateBattleByMatches(strategy, queueItems, battleReady, 7);
+            foreach (string tankClass in shuffledTankClasses)
+            {
+                // todo: remove or generalize this SameClass call
+                battleReady = PopulateBattleByMatches(new SameClass(tankClass), queueItems, battleReady, MaxTanksOfSameType);
+            }
 
             return battleReady;
         }
@@ -20,27 +32,8 @@ namespace MatchMaker
         {
             for (int i = 0; i < loopMax; i++)
             {
-                battleReady = strategy.PopulateBattle(queueItems, battleReady);
-            }
-
-            return battleReady;
-        }
-
-
-        public static IBattle CreateFillBattle(IStrategy strategy, IRandom random, QueueItems queueItems)
-        {
-            int maxTanksOfSameType = 3;
-            List<string> tankTypes = random.Shuffle(new List<string> { "Heavy", "Light", "TankDestroyer", "Medium" });
-
-            IBattle battleReady = new BattleReady();
-
-            foreach (string tankType in tankTypes)
-            {
-                if (battleReady.IsNotReadyToFight())
-                {                   
-                    // todo: remove or generalize this
-                    battleReady = PopulateBattleByMatches(new SameClass(tankType), queueItems, battleReady, maxTanksOfSameType);
-                }
+                if (battleReady.IsNotReadyToFight()) battleReady = strategy.PopulateBattle(queueItems, battleReady);
+              //  battleReady = strategy.PopulateBattle(queueItems, battleReady);
             }
 
             return battleReady;
